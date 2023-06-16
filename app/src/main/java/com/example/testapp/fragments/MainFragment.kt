9370f2f9d623971d6942a59e1f0d5c1b0d1b3753
shumslav <shum.slav@yandex.ru>
2023-06-16 21:30:40 +0500

    Added bottom navigation

---
+++ app/src/main/java/com/example/testapp/fragments/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.example.testapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.testapp.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
